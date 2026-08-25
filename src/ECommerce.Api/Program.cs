0f104686a0f8aa7cfc68a11c9143c07aff01cc53
Ahmed Rabie <deeva7med@gmail.com>
2026-08-25 15:36:02 +0300

    Build API foundation

--- src/ECommerce.Api/Program.cs
+++ src/ECommerce.Api/Program.cs
@@ -1,41 +1,33 @@
+using ECommerce.Api.Middleware;
+using ECommerce.Modules.Catalog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// add services
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
+
+builder.Services.AddCatalogModule();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// middleware pipeline
+app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "ECommerce API v1");
+    });
 }
 
-app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapHealthChecks("/health");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ src/ECommerce.Modules.Catalog/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Modules.Catalog
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCatalogModule(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+    }
+}
